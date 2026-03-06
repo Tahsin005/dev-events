ab2387af0bafbee1a403e30002fd87eab086836e
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-03-07 01:18:04 +0600

    feat: add event listing feature with EventCard and ExploreBtn, introduce Navbar, integrate featured events on Home page, define event data structure, and update global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+import EventCard from "@/components/EventCard"
+import ExploreBtn from "@/components/ExploreBtn"
+import events from "@/lib/constants"
+
 const Home = () => {
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev <br /> Event You Mustn't Miss</h1>
       <p className="text-center mt-5">Hackathons, Meetups, Conferences, All in one place!</p>
+
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h1>Featured Events</h1>
+
+        <ul className="events">
+                    {events && events.length > 0 && events.map((event) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+      </div>
     </section>
   )
 }
