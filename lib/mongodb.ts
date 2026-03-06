e154389242c417645d3dc2d74acd05bcf56c5926
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-03-07 04:52:25 +0600

    fix: update key for db uri environment variable to not use  NEXT_PUBLIC_ prefix to expose the db uri to the client

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -14,11 +14,11 @@
 
 import mongoose, { ConnectOptions, Mongoose } from 'mongoose'
 
-const MONGODB_URI = process.env.NEXT_PUBLIC_DB_URL
+const MONGODB_URI = process.env.MONGODB_URI
 
 if (!MONGODB_URI) {
   throw new Error(
-    'Please define the NEXT_PUBLIC_DB_URL environment variable inside .env.local'
+    'Please define the MONGODB_URI environment variable inside .env.local'
   )
 }
 
