cd813c0cc83013df7095f054a94f0ca0c0bb6bc4
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-03-07 14:49:51 +0600

    feat: add event details page with agenda, tags, and booking functionality

---
+++ components/BookEvent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BookEvent = ({ eventId, slug }: { eventId: string, slug: string;}) => {
+  return (
+    <div>
+      Book Event
+    </div>
+  )
+}
+
+export default BookEvent
